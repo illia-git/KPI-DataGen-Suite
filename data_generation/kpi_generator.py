5e0ed86cd6f68a607676a80dd3dc11b97bb68644
illia-git <ichykurov@gmail.com>
2023-11-16 01:11:05 +0100

    fix: compatibility with looker studio

--- data_generation/kpi_generator.py
+++ data_generation/kpi_generator.py
@@ -11,31 +11,23 @@ def generate_kpi_data(num_records):
         dict: Dictionary containing KPI data.
     """
     return {
-        "Total Visits": np.random.randint(800, 1500, num_records),
-        "Unique Visitors": np.random.randint(700, 1200, num_records),
-        "Avg Session Duration": np.round(np.random.uniform(3, 10, num_records), 2),
-        "Bounce Rate": np.round(np.random.uniform(20, 70, num_records), 2),
-        "Journeys Per Session": np.random.randint(1, 5, num_records),
-        "Avg Time on Journey": np.round(np.random.uniform(1, 5, num_records), 2),
-        "Most Visited Journey": np.random.choice(
-            ["Car Configurator", "Dealer Search", "Inventory Check"], num_records
-        ),
-        "Conversion Rate": np.round(np.random.uniform(1, 10, num_records), 2),
-        "Lead Conversion Rate": np.round(np.random.uniform(0.5, 5, num_records), 2),
-        "Document Downloads": np.random.randint(50, 200, num_records),
-        "Purchases": np.random.randint(5, 30, num_records),
-        "Feedback Score": np.round(np.random.uniform(3, 5, num_records), 1),
-        "Search Query Frequency": np.random.choice(
-            ["new car models", "car pricing", "dealership locations"], num_records
-        ),
-        "Search Result CTR": np.round(np.random.uniform(20, 50, num_records), 2),
-        "Social Shares": np.random.randint(40, 100, num_records),
-        "Page Load Time": np.round(np.random.uniform(1, 4, num_records), 2),
-        "Error Rates": np.round(np.random.uniform(0.01, 0.1, num_records), 2),
-        "User Demographics": np.random.choice(
-            ["25-34, Male", "35-44, Female", "18-24, Female"], num_records
-        ),
-        "Behavioral Segment": np.random.choice(
-            ["Returning Visitor", "New Visitor", "Frequent Buyer"], num_records
-        )
+        "Total Visits": [random.randint(800, 1500) for _ in range(num_records)],
+        "Unique Visitors": [random.randint(700, 1200) for _ in range(num_records)],
+        "Avg Session Duration": [round(random.uniform(3, 10), 2) for _ in range(num_records)],
+        "Bounce Rate": [round(random.uniform(20, 70), 2) for _ in range(num_records)],
+        "Journeys Per Session": [random.randint(1, 5) for _ in range(num_records)],
+        "Avg Time on Journey": [round(random.uniform(1, 5), 2) for _ in range(num_records)],
+        "Most Visited Journey": [random.choice(["Car Configurator", "Dealer Search", "Inventory Check"]) for _ in range(num_records)],
+        "Conversion Rate": [round(random.uniform(1, 10), 2) for _ in range(num_records)],
+        "Lead Conversion Rate": [round(random.uniform(0.5, 5), 2) for _ in range(num_records)],
+        "Document Downloads": [random.randint(50, 200) for _ in range(num_records)],
+        "Purchases": [random.randint(5, 30) for _ in range(num_records)],
+        "Feedback Score": [round(random.uniform(3, 5), 1) for _ in range(num_records)],
+        "Search Query Frequency": [random.choice(["new car models", "car pricing", "dealership locations"]) for _ in range(num_records)],
+        "Search Result CTR": [round(random.uniform(20, 50), 2) for _ in range(num_records)],
+        "Social Shares": [random.randint(40, 100) for _ in range(num_records)],
+        "Page Load Time": [round(random.uniform(1, 4), 2) for _ in range(num_records)],
+        "Error Rates": [round(random.uniform(0.01, 0.1), 2) for _ in range(num_records)],
+        "User Demographics": [random.choice(["25-34, Male", "35-44, Female", "18-24, Female"]) for _ in range(num_records)],
+        "Behavioral Segment": [random.choice(["Returning Visitor", "New Visitor", "Frequent Buyer"]) for _ in range(num_records)]
     }
